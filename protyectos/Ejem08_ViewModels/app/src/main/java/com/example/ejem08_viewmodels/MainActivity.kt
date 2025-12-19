f56ba10a7bc3a09148dd2df9d68eb4fcae5604ea
Nicolás <nicolas.jove.cubillo@gregoriofer.com>
2025-12-19 13:06:19 +0100

    ultiama clase del año

--- protyectos/Ejem08_ViewModels/app/src/main/java/com/example/ejem08_viewmodels/MainActivity.kt
+++ protyectos/Ejem08_ViewModels/app/src/main/java/com/example/ejem08_viewmodels/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.ejem08_viewmodels
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ejem08_viewmodels.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -26,7 +29,29 @@ class MainActivity : AppCompatActivity() {
             binding.cont = contadorViewModel
             cNormal.incrementar()
             binding.contNormal = cNormal
-        }
 
+            // Funcion que recive por argumento otra funcion
+            if (cNormal.contador>5){
+                lanzarSaludo {conToast()}
+            }else{
+                lanzarSaludo {conSnackBar()}
+            }
+
+        }
     }
+
+    // ej: Cuando llamas con Toast -> nombrePersonalizado se remplazara por la funcion conToast()
+    // con -> Unit decimos que es de tipo funcion
+    private fun lanzarSaludo(nombrePersonalizado:()-> Unit){
+        // desarroyo de la funcion
+        Log.d("depurando","Depurando saludo...")
+        nombrePersonalizado()
+    }
+    private fun conToast() {
+        Toast.makeText(this, "Preparando el saludo", Toast.LENGTH_SHORT).show()
+    }
+    private fun conSnackBar(){
+        Snackbar.make(binding.root, "Preparando el saludo", Snackbar.LENGTH_SHORT).show()
+    }
+
 }
---
+++ protyectos/Ejem08_ViewModels/app/src/main/java/com/example/ejem08_viewmodels/MainActivity2.kt
@@ -0,0 +1,24 @@
+package com.example.ejem08_viewmodels
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.ejem08_viewmodels.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMain2Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
